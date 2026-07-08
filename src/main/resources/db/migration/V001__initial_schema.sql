f12666ce8a83e60198472b6adba8024a9debf8e2
Eduardo Victor <eduardodejesus.dev@hotmail.com>
2026-07-07 22:46:03 -0300

    setup: initial Flyway migration structure

---
+++ src/main/resources/db/migration/V001__initial_schema.sql
@@ -0,0 +1,11 @@
+-- Initial schema placeholder
+-- Real migrations will be added as features are built
+
+CREATE SCHEMA IF NOT EXISTS fintrackerhub;
+
+-- Future tables will go here:
+-- - users
+-- - accounts
+-- - transactions
+-- - categories
+-- - compromissos_financeiros
